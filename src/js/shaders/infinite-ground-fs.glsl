6375a61759ad578ab1f608692d010f86a378cec0
bcarlborg <bcarlborg@gmail.com>
2019-11-15 15:40:34 +0100

    close to infinite gronud

--- src/js/shaders/infinite-ground-fs.glsl
+++ src/js/shaders/infinite-ground-fs.glsl
@@ -2,10 +2,12 @@ ShaderSource.source[document.currentScript.src.split('js/shaders/')[1]] = `#vers
   precision mediump float;
 
   in vec2 v_texcoord;
+  in vec4 v_homogeneous;
+
   uniform sampler2D u_textureImage;
   out vec4 outColor;
 
   void main() {
-    outColor = texture(u_textureImage, v_texcoord);
+    outColor = texture(u_textureImage, v_homogeneous.xy / v_homogeneous.w);
   }
 `;
--- src/js/shaders/infinite-ground-vs.glsl
+++ src/js/shaders/infinite-ground-vs.glsl
@@ -1,15 +1,16 @@
 ShaderSource.source[document.currentScript.src.split('js/shaders/')[1]] = `#version 300 es
   in vec3 a_position;
-  in vec2 a_texcoord;
+  in vec4 a_homogeneous;
 
   uniform mat4 worldMatrix;
   uniform mat4 viewMatrix;
   uniform mat4 projectionMatrix;
 
   out vec2 v_texcoord;
+  out vec4 v_homogeneous;
 
   void main() {
-    v_texcoord = a_texcoord;
+    v_homogeneous = a_homogeneous;
     gl_Position = projectionMatrix * viewMatrix * worldMatrix * vec4(a_position, 1.0);
   }
 `;
