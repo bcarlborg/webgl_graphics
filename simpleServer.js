9e8f453dc07c3a0024bbcfdf4945dc2f01344dcb
bcarlborg <bcarlborg@gmail.com>
2019-11-19 15:03:16 +0100

    testing some nonesense out

--- simpleServer.js
+++ simpleServer.js
@@ -2,15 +2,16 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || 4000;
 
-console.log('your site is running at port: ', port);
-app.use(express.static(__dirname + '/src'));
-app.use(express.static(__dirname + '/src/lib'));
-app.use(express.static(__dirname + '/src/media'));
-app.use(express.static(__dirname + '/src/js'));
-app.use(express.static(__dirname + '/src/js/helpers'));
-app.use(express.static(__dirname + '/src/js/games/orbits'));
-app.use(express.static(__dirname + '/src/js/games'));
-app.use(express.static(__dirname + '/src/js/shaders'));
-app.use(express.static(__dirname + '/src/js/shaders/procedural'));
+// console.log('your site is running at port: ', port);
+
+app.use('static', express.static(__dirname + '/src'));
+app.use('static', express.static(__dirname + '/src/lib'));
+app.use('static', express.static(__dirname + '/src/media'));
+app.use('static', express.static(__dirname + '/src/js'));
+app.use('static', express.static(__dirname + '/src/js/helpers'));
+app.use('static', express.static(__dirname + '/src/js/games/orbits'));
+app.use('static', express.static(__dirname + '/src/js/games'));
+app.use('static', express.static(__dirname + '/src/js/shaders'));
+app.use('static', express.static(__dirname + '/src/js/shaders/procedural'));
 
 app.listen(port);
--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,5 @@
+import Mesh from './Mesh.js';
+import Game from './games/Game.js';
 import Scene from './Scene.js';
 
 export default class App {
