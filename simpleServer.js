b538a5e243c7cc3f1db826467834d226fe90de19
bcarlborg <bcarlborg@gmail.com>
2019-11-19 14:55:24 +0100

    removing path

--- simpleServer.js
+++ simpleServer.js
@@ -3,14 +3,14 @@ app = express(),
 port = process.env.PORT || 4000;
 
 console.log('your site is running at port: ', port);
-app.use(express.static(path.join(__dirname, '/src')));
-app.use(express.static(path.join(__dirname, '/src/lib')));
-app.use(express.static(path.join(__dirname, '/src/media')));
-app.use(express.static(path.join(__dirname, '/src/js')));
-app.use(express.static(path.join(__dirname, '/src/js/helpers')));
-app.use(express.static(path.join(__dirname, '/src/js/games/orbits')));
-app.use(express.static(path.join(__dirname, '/src/js/games')));
-app.use(express.static(path.join(__dirname, '/src/js/shaders')));
-app.use(express.static(path.join(__dirname, '/src/js/shaders/procedural')));
+app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/src/lib'));
+app.use(express.static(__dirname + '/src/media'));
+app.use(express.static(__dirname + '/src/js'));
+app.use(express.static(__dirname + '/src/js/helpers'));
+app.use(express.static(__dirname + '/src/js/games/orbits'));
+app.use(express.static(__dirname + '/src/js/games'));
+app.use(express.static(__dirname + '/src/js/shaders'));
+app.use(express.static(__dirname + '/src/js/shaders/procedural'));
 
 app.listen(port);
