a1fa3febe05dd8d521690693f7691192216a39ab
bcarlborg <bcarlborg@gmail.com>
2019-11-19 14:39:42 +0100

    all sub dirs

--- simpleServer.js
+++ simpleServer.js
@@ -6,5 +6,10 @@ app.use(express.static(__dirname + '/src'));
 app.use(express.static(__dirname + '/src/lib'));
 app.use(express.static(__dirname + '/src/media'));
 app.use(express.static(__dirname + '/src/js'));
+app.use(express.static(__dirname + '/src/js/helpers'));
+app.use(express.static(__dirname + '/src/js/games'));
+app.use(express.static(__dirname + '/src/js/games/orbits'));
+app.use(express.static(__dirname + '/src/js/shaders'));
+app.use(express.static(__dirname + '/src/js/shaders/procedural'));
 
 app.listen(port);
