6538c133d387b8a0afc84dfeb8c71bf7bd2f3e15
bcarlborg <bcarlborg@gmail.com>
2019-12-02 23:17:09 +0100

    snowman is moving baby

--- src/js/ClippedQuadric.js
+++ src/js/ClippedQuadric.js
@@ -7,6 +7,10 @@ export default class ClippedQuadric extends SceneGraphEntity {
     super();
     this.index = index;
     this.globalUniforms = new GlobalUniforms();
+    this.transformMatrix = glMatrix.mat4.create();
+    this.surface = glMatrix.mat4.create();
+    this.clipper = glMatrix.mat4.create();
+
     this.uniforms = {
       surface: glMatrix.mat4.create(),
       clipper: glMatrix.mat4.create(),
@@ -15,14 +19,14 @@ export default class ClippedQuadric extends SceneGraphEntity {
 
   setToUnitCylinder() {
     glMatrix.mat4.set(
-      this.uniforms.surface,
+      this.surface,
       1, 0, 0, 0,
       0, 0, 0, 0,
       0, 0, 1, 0,
       0, 0, 0, -1,
     );
     glMatrix.mat4.set(
-      this.uniforms.clipper,
+      this.clipper,
       0, 0, 0, 0,
       0, 1, 0, 0,
       0, 0, 0, 0,
@@ -30,8 +34,42 @@ export default class ClippedQuadric extends SceneGraphEntity {
     );
   }
 
+  transformSurfaceMatrix() {
+    const transformableProps = ['clipper', 'surface'];
+
+    transformableProps.forEach((prop) => {
+      glMatrix.mat4.copy(
+        this.transformMatrix,
+        this.worldMatrix,
+      );
+
+      glMatrix.mat4.invert(
+        this.transformMatrix,
+        this.transformMatrix,
+      );
+
+      glMatrix.mat4.multiply(
+        this.uniforms[prop],
+        this[prop],
+        this.transformMatrix,
+      );
+
+      glMatrix.mat4.transpose(
+        this.transformMatrix,
+        this.transformMatrix,
+      );
+
+      glMatrix.mat4.multiply(
+        this.uniforms[prop],
+        this.transformMatrix,
+        this.uniforms[prop],
+      );
+    });
+  }
+
   update() {
     super.update();
+    this.transformSurfaceMatrix();
     this.globalUniforms.setBlockUniforms(
       `clippedQuadrics[${this.index}]`, this.uniforms,
     );
--- src/js/games/casting/objects/Snowman.js
+++ src/js/games/casting/objects/Snowman.js
@@ -1,8 +1,23 @@
 import ClippedQuadric from '../../../ClippedQuadric.js';
+import GameTime from '../../../GameTime.js';
 
 export default class Snowman extends ClippedQuadric {
   constructor(index) {
     super(index);
     this.setToUnitCylinder();
+    this.timeInfo = (new GameTime()).timeInfo;
+    this.moveAlongUp(10);
+  }
+
+  hop() {
+    const upOrDown = Math.sin(this.timeInfo.t / 1000);
+    let vertDelta = this.timeInfo.dt / 1000;
+    if (upOrDown < 0) vertDelta = -1 * this.timeInfo.dt / 1000;
+    this.moveAlongUp(vertDelta);
+  }
+
+  update() {
+    // this.hop();
+    super.update();
   }
 }
