711080ed65ce20abbfe1e9bb4009468a3a043b8e
bcarlborg <bcarlborg@gmail.com>
2019-12-06 19:05:23 +0100

    perlin noise in the fragmen shader but acting a bit wonky

--- src/js/shaders/perlin-vs.glsl
+++ src/js/shaders/perlin-vs.glsl
@@ -5,6 +5,7 @@ ShaderSource.source[document.currentScript.src.split('js/shaders/')[1]] = `#vers
   in vec4 a_color;
   in vec4 a_homogeneous;
 
+
   struct cameraStruct {
     mat4 viewMatrix;
     mat4 projectionMatrix;
@@ -16,9 +17,11 @@ ShaderSource.source[document.currentScript.src.split('js/shaders/')[1]] = `#vers
   uniform mat4 u_worldMatrix;
 
   out vec4 v_fragmentColor;
+  out vec3 v_vertexPosition;
 
   void main() {
     v_fragmentColor = a_color;
     gl_Position = camera.projectionMatrix * camera.viewMatrix * u_worldMatrix * a_position;
+    v_vertexPosition = gl_Position.xyz;
   }
 `;
